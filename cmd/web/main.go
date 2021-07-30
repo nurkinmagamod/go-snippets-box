30764ebf3872ade613da8c2dea1a6f04590dee07
nurkinmagamod <battchat@gmail.com>
2021-07-30 11:56:31 +0300

    Added main project files

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module nurkinmagazmod/snippetbox
+
+go 1.16
