08d721419be67a68ab46799454c12c1c847c6acf
nurkinmagamod <battchat@gmail.com>
2021-07-31 15:31:55 +0300

    Added changed handlers and main for handle new templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
-	// Register the two new handler functions and corresponding URL patterns with
-	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
