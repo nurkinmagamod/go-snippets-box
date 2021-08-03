32c4d9d6a71a64a12b62d2465675a708272912bf
Azamat <azamat.nurkin@smddev.com>
2021-08-03 17:23:54 +0300

    Revert "Added error handling helpers"
    This reverts commit 2a2bff0959c7aa4c18a9d16e8722e527c6363614.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
 
@@ -21,12 +21,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.serverError(w, err)
+		app.errorLog.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.serverError(w, err)
+		app.errorLog.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
@@ -34,7 +36,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
@@ -44,7 +46,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
+		http.Error(w, "Method not Allowed", 405)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
